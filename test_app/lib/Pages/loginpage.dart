f88ed36c68f8691fa798711eeed65f667aaab7a6
Rohit Kale <rohitkale523@gmail.com>
2024-02-20 20:20:46 +0530

    dart fix applied

--- test_app/lib/Pages/loginpage.dart
+++ test_app/lib/Pages/loginpage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _firebase = FirebaseAuth.instance;
 
